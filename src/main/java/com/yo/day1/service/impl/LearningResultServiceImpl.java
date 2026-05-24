a6cf5af35600aafe9e7d7fe2521dfcb8f4c31522
Hoàng Đức Quang <hdquang6@gmail.com>
2026-05-24 13:50:13 +0700

    add new futures 2

---
+++ src/main/java/com/yo/day1/service/impl/LearningResultServiceImpl.java
@@ -0,0 +1,10 @@
+package com.yo.day1.service.impl;
+
+import com.yo.day1.service.LearningResultService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LearningResultServiceImpl implements LearningResultService {
+}
